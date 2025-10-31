1b86d11248af70e23286062de803559a2c2452fe
Pascal Schade <pascal@zauberzeug.com>
2025-10-31 15:35:10 +0100

    move over status_control and can_open_master

--- feldfreund_devkit/hardware/__init__.py
+++ feldfreund_devkit/hardware/__init__.py
@@ -1,5 +1,9 @@
+from .can_open_master import CanOpenMasterHardware
+from .status_control import StatusControlHardware
 from .teltonika_router import TeltonikaRouter
 
 __all__ = [
+    'CanOpenMasterHardware',
+    'StatusControlHardware',
     'TeltonikaRouter',
 ]
