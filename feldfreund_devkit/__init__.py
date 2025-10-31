f892c50afebba98f1ec50af35a7803cc09d95e79
Pascal Schade <pascal@zauberzeug.com>
2025-10-31 11:49:59 +0100

    move logging over

--- feldfreund_devkit/__init__.py
+++ feldfreund_devkit/__init__.py
@@ -1,4 +1,9 @@
+from . import log_configuration
 from .entity_locator import EntityLocator
 from .system import System
 
-__all__ = ['EntityLocator', 'System']
+__all__ = [
+    'EntityLocator',
+    'System',
+    'log_configuration',
+]
