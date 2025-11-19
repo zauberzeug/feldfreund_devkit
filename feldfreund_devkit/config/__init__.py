f67b9166494fa6f554bb557af22c70b9f0a6804d
Pascal Schade <pascal@zauberzeug.com>
2025-11-19 03:05:04 +0100

    reload config

--- feldfreund_devkit/config/__init__.py
+++ feldfreund_devkit/config/__init__.py
@@ -18,6 +18,7 @@ def get_config(robot_name: str) -> FeldfreundConfiguration:
     try:
         module_name = f'config.{robot_name.lower()}'
         config_module = importlib.import_module(module_name)
+        importlib.reload(config_module)  # reload to avoid cached config
         return config_module.config
     except ImportError as e:
         raise RuntimeError(f'No configuration found for robot: {robot_name}') from e
