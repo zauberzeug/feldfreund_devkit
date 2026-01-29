497f094a47bf6435f71a159bd0fcfdbe94c365c2
Pascal Schade <pascal@zauberzeug.com>
2026-01-29 15:26:56 +0100

    adjust RobotBrainConfiguration

--- feldfreund_devkit/config/robot_brain_configuration.py
+++ feldfreund_devkit/config/robot_brain_configuration.py
@@ -7,9 +7,20 @@ class RobotBrainConfiguration:
 
     Defaults:
         enable_esp_on_startup: False
-        use_espresso: True
+        nand: False
+        swap_pins: False
     """
     name: str
-    flash_params: list[str]
     enable_esp_on_startup: bool = False
-    use_espresso: bool = True
+    # https://github.com/zauberzeug/lizard/blob/main/espresso.py
+    nand: bool = False
+    swap_pins: bool = False
+
+    @property
+    def flash_params(self) -> list[str]:
+        params = []
+        if self.nand:
+            params.append('--nand')
+        if self.swap_pins:
+            params.append('--swap_pins')
+        return params
